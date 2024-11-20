07f2551d471994202130fd9baa2ad50141004507
paulinic <nicole.pauli@stud.unilu.ch>
2024-11-20 13:22:48 +0100

    Add files via upload

---
+++ R-Script.R
@@ -0,0 +1,14 @@
+#load libraries and data 
+library(tidyverse)
+library(ggplot2)
+library(dplyr)
+#set seed for reproducibility
+set.seed(123)
+
+#read data
+csv_data <- read.csv("~/Documents/Uni_Luzern_LUMACSS/HS24/Introduction to R /my-awesome-r-project/data_main.rds")
+str(csv_data)
+
+#text
+
+#dies ist ein Commit Testing
